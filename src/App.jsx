6c980fa0d4c6a01926aa70169b01cdba3ae7265d
hudanadeem <hudanadeem.02@gmail.com>
2025-02-12 18:35:25 -0500

    Footer styling complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header />
     <Hero/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import './Footer.scss';
+import instagramIcon from '../../assets/images/Instagram.svg';
+import twitterIcon from '../../assets/images/X_twitter.svg';
+import facebookIcon from '../../assets/images/Facebook.svg';
+import pinterestIcon from '../../assets/images/Pinterest.svg'
+
+
+
+const Footer = () =>{
+    return(
+        <>
+        <div className='footer'>
+            <h2>Snaps</h2>
+            <div className='footer__links--1'>
+                <h3><a href="#">For photographers</a></h3>
+                <h3><a href="#">Hire Talent</a></h3>
+                <h3><a href="#">Insipration</a></h3>
+            </div>
+            <div className='footer__links--2'>
+                <h3><a href="#">About</a></h3>
+                <h3><a href="#">Careers</a></h3>
+                <h3><a href="#">Support</a></h3>
+            </div>
+            <div className='footer__icon--container'>
+                <a href = "https://www.facebook.com/">
+                    <img className = "footer__icon" src={facebookIcon} alt="facebook"></img>
+                </a>
+                <a href = "https://www.x.com/">
+                    <img className = "footer__icon" src={twitterIcon} alt="twitter"></img>
+                </a>
+                <a href = "https://www.instagram.com/">
+                    <img className = "footer__icon" src={instagramIcon} alt="instagram"></img>
+                </a>
+                <a href = "https://www.pinterest.com/">
+                    <img className = "footer__icon" src={pinterestIcon} alt="pinterest"></img>
+                </a>
+            </div>
+            <div className='footer__mark'>
+                <p>@ 2024 Snaps</p>
+                <p>Terms</p>
+                <p>Privacy</p>
+                <p>Cookies</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Footer;

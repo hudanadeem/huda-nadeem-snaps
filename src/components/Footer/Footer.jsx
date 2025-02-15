a7c937025b63343e8bbd3e3d8cc469ed8f308332
hudanadeem <hudanadeem.02@gmail.com>
2025-02-15 16:53:52 -0500

    desktop view 3.1 complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,34 +10,36 @@ const Footer = () =>{
     return(
         <>
         <div className='footer'>
-            <div className='footer__links--conatiner'>
-                <h2>Snaps</h2>
-                <div className='footer__links'>
-                    <div className='footer__links--1'>
-                        <h3><a href="#">For photographers</a></h3>
-                        <h3><a href="#">Hire Talent</a></h3>
-                        <h3><a href="#">Insipration</a></h3>
-                    </div>
-                    <div className='footer__links--2'>
-                        <h3><a href="#">About</a></h3>
-                        <h3><a href="#">Careers</a></h3>
-                        <h3><a href="#">Support</a></h3>
+            <div className='footer__container'>
+                <div className='footer__links--conatiner'>
+                    <h2>Snaps</h2>
+                    <div className='footer__links'>
+                        <div className='footer__links--1'>
+                            <h3><a href="#">For photographers</a></h3>
+                            <h3><a href="#">Hire Talent</a></h3>
+                            <h3><a href="#">Insipration</a></h3>
+                        </div>
+                        <div className='footer__links--2'>
+                            <h3><a href="#">About</a></h3>
+                            <h3><a href="#">Careers</a></h3>
+                            <h3><a href="#">Support</a></h3>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='footer__icon--container'>
-                <a href = "https://www.facebook.com/">
-                    <img className = "footer__icon" src={facebookIcon} alt="facebook"></img>
-                </a>
-                <a href = "https://www.x.com/">
-                    <img className = "footer__icon" src={twitterIcon} alt="twitter"></img>
-                </a>
-                <a href = "https://www.instagram.com/">
-                    <img className = "footer__icon" src={instagramIcon} alt="instagram"></img>
-                </a>
-                <a href = "https://www.pinterest.com/">
-                    <img className = "footer__icon" src={pinterestIcon} alt="pinterest"></img>
-                </a>
+                <div className='footer__icon--container'>
+                    <a href = "https://www.facebook.com/">
+                        <img className = "footer__icon" src={facebookIcon} alt="facebook"></img>
+                    </a>
+                    <a href = "https://www.x.com/">
+                        <img className = "footer__icon" src={twitterIcon} alt="twitter"></img>
+                    </a>
+                    <a href = "https://www.instagram.com/">
+                        <img className = "footer__icon" src={instagramIcon} alt="instagram"></img>
+                    </a>
+                    <a href = "https://www.pinterest.com/">
+                        <img className = "footer__icon" src={pinterestIcon} alt="pinterest"></img>
+                    </a>
+                </div>
             </div>
             <div className='footer__mark'>
                 <p>@ 2024 Snaps</p>

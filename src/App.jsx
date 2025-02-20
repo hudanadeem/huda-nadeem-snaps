c931d1f88c643996e90cf05b89261e7ffef92706
hudanadeem <hudanadeem.02@gmail.com>
2025-02-20 08:58:52 -0500

    added router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,18 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import PhotoList from "./components/PhotoList/PhotoList";
-import Footer from "./components/Footer/Footer";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import HomePage from "./pages/Home/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PhotoPage from "./pages/Photo/PhotoPage";
+
 
 function App() {
-  const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
-  const API_KEY = "?api_key=325ecbce-cb56-4c39-8333-877b3e1bfb3b";
-  const [photos, setPhotos] = useState([]);
-  const [tags, setTags] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${API_URL}/photos${API_KEY}`)
-      .then((response) => {
-        setPhotos(response.data);        
-      })
-      .catch((error) => {
-        console.error("Error fetching comments:", error);
-      });
-  }, []);
-
-  useEffect(() => {
-    axios.get(`${API_URL}/tags${API_KEY}`)
-      .then((response) => {
-        setTags(response.data);        
-      })
-      .catch((error) => {
-        console.error("Error fetching comments:", error);
-      });
-  }, []);
-
-
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [selectedTag, setSelectedTag] = useState(null);
-  const filteredPhotos = selectedTag
-    ? photos.filter((photo) => photo.tags.includes(selectedTag))
-    : photos;
 
   return (
-    <>
-      <Header
-        setDrawerOpen={setDrawerOpen}
-        drawerOpen={drawerOpen}
-        selectedTag={selectedTag}
-        setSelectedTag={setSelectedTag}
-        tags={tags}
-      />
-      <Hero drawerOpen={drawerOpen} />
-      <PhotoList filteredPhotos={filteredPhotos} drawerOpen={drawerOpen} />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="photo" element={<PhotoPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Photo/PhotoPage.jsx
@@ -0,0 +1,12 @@
+import "./PhotoPage.scss";
+
+function PhotoPage() {
+
+  return (
+    <div>
+        hi this is photo page
+    </div>
+  );
+}
+
+export default PhotoPage;
